56d010f8106241002ff10b5a957aa25e8d64b331
Greg Young <gregoryyoung1@gmail.com>
2014-03-01 16:11:42 +0200

    add hashes

---
+++ BloomBurger/BloomFilter.cs
@@ -0,0 +1,14 @@
+﻿using System.IO.MemoryMappedFiles;
+
+namespace BloomBurger
+{
+    public class BloomFilter
+    {
+        private MemoryMappedFile _file;
+
+        public BloomFilter(bool memoryOnly, long size, string filename)
+        {
+            
+        }
+    }
+}
---
+++ BloomBurger/Hashes/IHasher.cs
@@ -0,0 +1,9 @@
+﻿namespace BloomBurger.Hashes
+{
+    public interface IHasher
+    {
+        uint Hash(string s);
+        uint Hash(byte[] data);
+        uint Hash(byte[] data, int offset, uint len, uint seed);
+    }
+}
